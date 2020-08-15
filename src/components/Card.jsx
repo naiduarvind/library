cb9e7744485cac56626f01d7e4566c8b25d897f4
Arvind Naidu <arvind@thebility.engineer>
2020-08-15 23:55:15 +0800

    remove blue bg from book cover
    Signed-off-by: Arvind Naidu <arvind@thebility.engineer>

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -21,7 +21,7 @@ export const Card = props => {
   return (
     <div className="bg-white h-full shadow-sm rounded-md overflow-hidden hover:bg-blue-100">
       <Link to={`/${slug}`} state={{ navigation }} asModal>
-        <div className="bg-blue-300">
+        <div>
           <Img fixed={cover.childImageSharp.fixed} alt={name} />
         </div>
         <div className="p-5 pb-1">
